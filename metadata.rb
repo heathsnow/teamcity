86fc09c57c5fe2ef583bb90d9396826d713f99fd
heathsnow <heath.snow@changepoint.com>
2016-10-05 13:41:03 -0700

    rubocop update

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,8 @@ description      'Setup TeamCity agents and pull artifacts'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 ver_path = File.join(File.dirname(__FILE__), 'version.txt')
 version ((IO.read(ver_path) if File.exists?(ver_path)) || '0.3.0').chomp
+issues_url 'https://github.com/daptiv/teamcity/issues'
+source_url 'https://github.com/daptiv/teamcity/'
 recommends       'java' # ~FC053
 recipe           'teamcity::agent', 'Installs an agent for a TeamCity server'
 depends          'chef-sugar'
