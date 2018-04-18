7b9cf47992f770ef58a6e7b714a999b4684c20d0
Heath Snow <heathsnow@gmail.com>
2018-04-18 11:33:19 -0700

    chef 13

--- recipes/agent_linux.rb
+++ recipes/agent_linux.rb
@@ -20,7 +20,7 @@ require 'digest/md5'
 node['teamcity']['agents'].each_with_index do |(name, agent), index| # multiple agents
   next if agent.nil? # support removing of agents
 
-  agent = Teamcity::Agent.new name, node
+  agent = Teamcity::Agent.new(name, node)
   agent.set_defaults
 
   unless agent.server_url?
@@ -30,31 +30,31 @@ node['teamcity']['agents'].each_with_index do |(name, agent), index| # multiple 
   end
 
   # Create the users' group
-  group agent.group do
+  group agent['group'] do
   end
 
   # Create the user
-  user agent.user do
+  user agent['user'] do
     comment 'TeamCity Agent' + agent.label(' ')
-    gid agent.group
-    home agent.home
+    gid agent['group']
+    home agent['home']
   end
 
   directory "directory #{index}" do
-    path agent.system_dir
-    user agent.user
-    group agent.group
+    path agent['system_dir']
+    user agent['user']
+    group agent['group']
     recursive true
     action :create
-    not_if { File.exists? agent.system_dir }
+    not_if { File.exists? agent['system_dir'] }
   end
 
-  server_hash = Digest::MD5.hexdigest(agent.server_url)
+  server_hash = Digest::MD5.hexdigest(agent['server_url'])
   install_file = "#{Chef::Config[:file_cache_path]}/teamcity-agent-#{server_hash}.zip"
-  installed_check = Proc.new { ::File.exists? "#{agent.system_dir}/bin" }
+  installed_check = Proc.new { ::File.exists? "#{agent['system_dir']}/bin" }
 
   remote_file install_file do
-    source agent.server_url + '/update/buildAgent.zip'
+    source agent['server_url'] + '/update/buildAgent.zip'
     mode 0555
     action :create_if_missing
     not_if &installed_check
@@ -66,28 +66,28 @@ node['teamcity']['agents'].each_with_index do |(name, agent), index| # multiple 
   end
 
   # is there a better approach?
-  execute "unzip #{install_file} -d #{agent.system_dir}" do
-    user agent.user
-    group agent.group
-    creates "#{agent.system_dir}/bin"
+  execute "unzip #{install_file} -d #{agent['system_dir']}" do
+    user agent['user']
+    group agent['group']
+    creates "#{agent['system_dir']}/bin"
     not_if &installed_check
   end
 
   # as of TeamCity 6.5.4 the zip does NOT contain the file mode
   %w{linux-x86-32 linux-x86-64 linux-ppc-64 }.each do |platform|
-    file ::File.join(agent.system_dir, 'launcher/bin/TeamCityAgentService-' + platform) do
+    file ::File.join(agent['system_dir'], 'launcher/bin/TeamCityAgentService-' + platform) do
       mode 0755
     end
   end
   %w{agent findJava install}.each do |script|
-    file ::File.join(agent.system_dir, 'bin', "#{script}.sh") do
+    file ::File.join(agent['system_dir'], 'bin', "#{script}.sh") do
       mode 0755
     end
   end
 
   # try to extract agent name + authenticationCode from file
-  agent_config = ::File.join agent.system_dir, 'conf', 'buildAgent.properties'
-  if (agent.name.nil? || agent.authorization_token.nil?) && ::File.readable?(agent_config)
+  agent_config = ::File.join agent['system_dir'], 'conf', 'buildAgent.properties'
+  if (agent['name'].nil? || agent['authorization_token'].nil?) && ::File.readable?(agent_config)
     settings = File.new(agent_config).readlines.map do |s|
       s.index('#') ? s.slice(0, s.index('#')).strip : s.strip  # remove comments
     end.reject do |s|
@@ -97,21 +97,21 @@ node['teamcity']['agents'].each_with_index do |(name, agent), index| # multiple 
       memento[key] = value
       memento
     end
-    if agent.name.nil? && !settings['name'].nil?
+    if agent['name'].nil? && !settings['name'].nil?
       Chef::Log.info "Setting agent (#{name})'s name to #{settings['name']}"
-      agent.name = settings['name']
+      agent['name'] = settings['name']
     end
-    if agent.authorization_token.nil? && !settings['authorizationToken'].nil?
+    if agent['authorization_token'].nil? && !settings['authorizationToken'].nil?
       Chef::Log.info "Setting agent (#{name})'s authorization_token"
-      agent.authorization_token = settings['authorizationToken']
+      agent['authorization_token'] = settings['authorizationToken']
     end
   end
 
   # buildAgent.properties (TeamCity will restart if this file is changed)
   template agent_config do
     source 'buildAgent.properties.erb'
-    user agent.user
-    user agent.group
+    user agent['user']
+    user agent['group']
     mode 0644
     variables agent.to_hash
   end
