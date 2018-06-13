ca9438aaca21a6ebc98a7c8026a70f208e749343
Linus Ruth <linus.ruth@changepoint.com>
2018-06-13 16:33:27 -0700

    chore: switch to ubuntu 18.04, fix service name

--- terraform/teamcity_agent/variables.tf
+++ terraform/teamcity_agent/variables.tf
@@ -8,14 +8,14 @@ variable "domain_name" {}
 variable "env_hostname_prefix" {}
 variable "instance_key_name" {}
 variable "instance_private_key" {}
-variable "teamcity_agent_ami_name" { default = "ubuntu_xenial_16.04" }
+variable "teamcity_agent_ami_name" { default = "ubuntu_18.04" }
 variable "teamcity_agent_ami_owners" { default = "147491244536" }
 variable "teamcity_agent_data_volume_size" { default = "60" }
 variable "teamcity_agent_instance_count" { default = "1" }
 variable "teamcity_agent_instance_type" { default = "t2.medium" }
 variable "teamcity_agent_instance_user" { default = "ubuntu" }
 variable "teamcity_agent_log_volume_size" { default = "10" }
-variable "teamcity_agent_service_name" { default = "teamcity_agent" }
+variable "teamcity_agent_service_name" { default = "teamcity-agent" }
 variable "remote_state_bucket" {}
 variable "remote_state_region" {}
 variable "secret_key_file" { default = "/etc/chef/encrypted_data_bag_secret" }
