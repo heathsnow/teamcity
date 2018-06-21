6238761e9849e0bd034c27016df6082e6adee58f
Linus Ruth <linus.ruth@changepoint.com>
2018-06-21 11:00:36 -0700

    chore: remove dead code, adjust indentation

--- chef/teamcity/recipes/agent_linux.rb
+++ chef/teamcity/recipes/agent_linux.rb
@@ -17,134 +17,130 @@
 #
 require 'digest/md5'
 
-# node['teamcity']['agents'].each_with_index do |(name, agent), index| # multiple agents
-  # next if agent.nil? # support removing of agents
+home = node['teamcity']['agents']['home'] || File.join('', 'home', node['teamcity']['agents']['user'])
+system_dir = File.expand_path node['teamcity']['agents']['system_dir'], home
+temp_dir = File.expand_path node['teamcity']['agents']['temp_dir'], system_dir
+work_dir = File.expand_path node['teamcity']['agents']['work_dir'], system_dir
+server_url = node['teamcity']['agents']['server_url']
+agent_name = node['teamcity']['agents']['name']
+agent_auth_token = node['teamcity']['agents']['authorization_token']
 
-  home = node['teamcity']['agents']['home'] || File.join('', 'home', node['teamcity']['agents']['user'])
-  system_dir = File.expand_path node['teamcity']['agents']['system_dir'], home
-  temp_dir = File.expand_path node['teamcity']['agents']['temp_dir'], system_dir
-  work_dir = File.expand_path node['teamcity']['agents']['work_dir'], system_dir
-  server_url = node['teamcity']['agents']['server_url']
-  agent_name = node['teamcity']['agents']['name']
-  agent_auth_token = node['teamcity']['agents']['authorization_token']
+unless server_url && !server_url.empty?
+  message = "You need to setup the server url for agent #{agent_name}"
+  Chef::Log.fatal(message)
+  raise message
+end
 
-  unless server_url && !server_url.empty?
-    message = "You need to setup the server url for agent #{agent_name}"
-    Chef::Log.fatal(message)
-    raise message
+# Create the users' group
+group node['teamcity']['agents']['group'] do
+end
+
+# Create the user
+user node['teamcity']['agents']['user'] do
+  comment 'TeamCity Agent'
+  gid node['teamcity']['agents']['group']
+  home home
+end
+
+directory "directory #{system_dir}" do
+  path system_dir
+  user node['teamcity']['agents']['user']
+  group node['teamcity']['agents']['group']
+  recursive true
+  action :create
+  not_if { File.exists? system_dir }
+end
+
+server_hash = Digest::MD5.hexdigest(server_url)
+install_file = "#{Chef::Config[:file_cache_path]}/teamcity-agent-#{server_hash}.zip"
+installed_check = Proc.new { ::File.exists? "#{system_dir}/bin" }
+
+remote_file install_file do
+  source server_url + '/update/buildAgent.zip'
+  mode 0555
+  action :create_if_missing
+  not_if &installed_check
+end
+
+package 'unzip' do
+  action :install
+  not_if &installed_check
+end
+
+# is there a better approach?
+execute "unzip #{install_file} -d #{system_dir}" do
+  user node['teamcity']['agents']['user']
+  group node['teamcity']['agents']['group']
+  creates "#{system_dir}/bin"
+  not_if &installed_check
+end
+
+# as of TeamCity 6.5.4 the zip does NOT contain the file mode
+%w{linux-x86-32 linux-x86-64 linux-ppc-64 }.each do |platform|
+  file ::File.join(system_dir, 'launcher/bin/TeamCityAgentService-' + platform) do
+    mode 0755
   end
-
-  # Create the users' group
-  group node['teamcity']['agents']['group'] do
+end
+%w{agent findJava install}.each do |script|
+  file ::File.join(system_dir, 'bin', "#{script}.sh") do
+    mode 0755
   end
+end
 
-  # Create the user
-  user node['teamcity']['agents']['user'] do
-    comment 'TeamCity Agent'
-    gid node['teamcity']['agents']['group']
-    home home
+# try to extract agent name + authenticationCode from file
+agent_config = ::File.join system_dir, 'conf', 'buildAgent.properties'
+if (agent_name.nil? || agent_auth_token.nil?) && ::File.readable?(agent_config)
+  settings = File.new(agent_config).readlines.map do |s|
+    s.index('#') ? s.slice(0, s.index('#')).strip : s.strip  # remove comments
+  end.reject do |s|
+    s.index('=').nil? # remove lines without =
+  end.inject({}) do |memento, line| # split on = and convert to hash
+    key, value = line.split '='
+    memento[key] = value
+    memento
   end
+  if agent_name.nil? && !settings['name'].nil?
+    Chef::Log.info "Setting agent (#{agent_name})'s name to #{settings['name']}"
+    node.override['teamcity']['agents']['name'] = settings['name']
+  end
+  if agent_auth_token.nil? && !settings['authorizationToken'].nil?
+    Chef::Log.info "Setting agent (#{agent_name})'s authorization_token"
+    node.override['teamcity']['agents']['authorization_token'] = settings['authorizationToken']
+  end
+end
 
-  directory "directory #{system_dir}" do
-    path system_dir
-    user node['teamcity']['agents']['user']
-    group node['teamcity']['agents']['group']
-    recursive true
-    action :create
-    not_if { File.exists? system_dir }
-  end
+# buildAgent.properties (TeamCity will restart if this file is changed)
+template agent_config do
+  source 'buildAgent.properties.erb'
+  user node['teamcity']['agents']['user']
+  user node['teamcity']['agents']['group']
+  mode 0644
+  variables(
+    server_url: server_url,
+    name: node['teamcity']['agents']['name'],
+    work_dir: work_dir,
+    temp_dir: temp_dir,
+    system_dir: system_dir,
+    own_address: node['teamcity']['agents']['own_address'],
+    own_port: node['teamcity']['agents']['own_port'],
+    authorization_token: node['teamcity']['agents']['authorization_token'],
+    system_properties: node['teamcity']['agents']['system_properties'],
+    env_properties: node['teamcity']['agents']['env_properties']
+  )
+end
 
-  server_hash = Digest::MD5.hexdigest(server_url)
-  install_file = "#{Chef::Config[:file_cache_path]}/teamcity-agent-#{server_hash}.zip"
-  installed_check = Proc.new { ::File.exists? "#{system_dir}/bin" }
+# create systemd service definition
+service_name = 'teamcity-agent'
+template "/etc/systemd/system/#{service_name}.service" do
+  source 'teamcity-agent.service.erb'
+  mode 0644
+  variables(
+    user: node['teamcity']['agents']['user'],
+    system_dir: system_dir
+  )
+end
 
-  remote_file install_file do
-    source server_url + '/update/buildAgent.zip'
-    mode 0555
-    action :create_if_missing
-    not_if &installed_check
-  end
-
-  package 'unzip' do
-    action :install
-    not_if &installed_check
-  end
-
-  # is there a better approach?
-  execute "unzip #{install_file} -d #{system_dir}" do
-    user node['teamcity']['agents']['user']
-    group node['teamcity']['agents']['group']
-    creates "#{system_dir}/bin"
-    not_if &installed_check
-  end
-
-  # as of TeamCity 6.5.4 the zip does NOT contain the file mode
-  %w{linux-x86-32 linux-x86-64 linux-ppc-64 }.each do |platform|
-    file ::File.join(system_dir, 'launcher/bin/TeamCityAgentService-' + platform) do
-      mode 0755
-    end
-  end
-  %w{agent findJava install}.each do |script|
-    file ::File.join(system_dir, 'bin', "#{script}.sh") do
-      mode 0755
-    end
-  end
-
-  # try to extract agent name + authenticationCode from file
-  agent_config = ::File.join system_dir, 'conf', 'buildAgent.properties'
-  if (agent_name.nil? || agent_auth_token.nil?) && ::File.readable?(agent_config)
-    settings = File.new(agent_config).readlines.map do |s|
-      s.index('#') ? s.slice(0, s.index('#')).strip : s.strip  # remove comments
-    end.reject do |s|
-      s.index('=').nil? # remove lines without =
-    end.inject({}) do |memento, line| # split on = and convert to hash
-      key, value = line.split '='
-      memento[key] = value
-      memento
-    end
-    if agent_name.nil? && !settings['name'].nil?
-      Chef::Log.info "Setting agent (#{agent_name})'s name to #{settings['name']}"
-      node.override['teamcity']['agents']['name'] = settings['name']
-    end
-    if agent_auth_token.nil? && !settings['authorizationToken'].nil?
-      Chef::Log.info "Setting agent (#{agent_name})'s authorization_token"
-      node.override['teamcity']['agents']['authorization_token'] = settings['authorizationToken']
-    end
-  end
-
-  # buildAgent.properties (TeamCity will restart if this file is changed)
-  template agent_config do
-    source 'buildAgent.properties.erb'
-    user node['teamcity']['agents']['user']
-    user node['teamcity']['agents']['group']
-    mode 0644
-    variables(
-      server_url: server_url,
-      name: node['teamcity']['agents']['name'],
-      work_dir: work_dir,
-      temp_dir: temp_dir,
-      system_dir: system_dir,
-      own_address: node['teamcity']['agents']['own_address'],
-      own_port: node['teamcity']['agents']['own_port'],
-      authorization_token: node['teamcity']['agents']['authorization_token'],
-      system_properties: node['teamcity']['agents']['system_properties'],
-      env_properties: node['teamcity']['agents']['env_properties']
-    )
-  end
-
-  # create systemd service definition
-  service_name = 'teamcity-agent'
-  template "/etc/systemd/system/#{service_name}.service" do
-    source 'teamcity-agent.service.erb'
-    mode 0644
-    variables(
-      user: node['teamcity']['agents']['user'],
-      system_dir: system_dir
-    )
-  end
-
-  service service_name do
-    action [:enable, :start]
-    supports :status => true
-  end
-# end
+service service_name do
+  action [:enable, :start]
+  supports :status => true
+end
--- chef/teamcity/recipes/agent_windows.rb
+++ chef/teamcity/recipes/agent_windows.rb
@@ -23,122 +23,114 @@ java_home = node.deep_fetch('teamcity', 'java_home') || node.deep_fetch('java', 
 java_exe = 'java'
 java_exe = ::File.join(java_home, 'bin', 'java.exe') if java_home
 
-# node['teamcity']['agents'].each do |name, agent| # multiple agents
-  # next if agent.nil? # support removing of agents
+home = node['teamcity']['agents']['home'] || File.join('', 'home', node['teamcity']['agents']['user'])
+system_dir = File.expand_path node['teamcity']['agents']['system_dir'], home
+temp_dir = File.expand_path node['teamcity']['agents']['temp_dir'], system_dir
+work_dir = File.expand_path node['teamcity']['agents']['work_dir'], system_dir
+server_url = node['teamcity']['agents']['server_url']
+agent_name = node['teamcity']['agents']['name']
+agent_auth_token = node['teamcity']['agents']['authorization_token']
 
-  home = node['teamcity']['agents']['home'] || File.join('', 'home', node['teamcity']['agents']['user'])
-  system_dir = File.expand_path node['teamcity']['agents']['system_dir'], home
-  temp_dir = File.expand_path node['teamcity']['agents']['temp_dir'], system_dir
-  work_dir = File.expand_path node['teamcity']['agents']['work_dir'], system_dir
-  server_url = node['teamcity']['agents']['server_url']
-  agent_name = node['teamcity']['agents']['name']
-  agent_auth_token = node['teamcity']['agents']['authorization_token']
+unless server_url && !server_url.empty?
+  message = "You need to setup the server url for agent #{agent_name}"
+  Chef::Log.fatal(message)
+  raise message
+end
 
-  unless server_url && !server_url.empty?
-    message = "You need to setup the server url for agent #{agent_name}"
-    Chef::Log.fatal(message)
-    raise message
+server_hash = Digest::MD5.hexdigest(server_url)
+install_file = "#{Chef::Config[:file_cache_path]}/teamcity-agent-#{server_hash}.zip"
+installed_check = Proc.new { ::File.exists? "#{system_dir}/bin" }
+
+directory system_dir do
+  recursive true
+  action :create
+end
+
+remote_file install_file do
+  source server_url + '/update/buildAgent.zip'
+  action :create_if_missing
+  not_if &installed_check
+end
+
+windows_zipfile system_dir do
+  source install_file
+  action :unzip
+  not_if &installed_check
+end
+
+# try to extract agent name + authenticationCode from file
+agent_config = ::File.join system_dir, 'conf', 'buildAgent.properties'
+if (agent_name.nil? || agent_auth_token.nil?) && ::File.readable?(agent_config)
+  settings = File.new(agent_config).readlines.map do |s|
+    s.index('#') ? s.slice(0, s.index('#')).strip : s.strip  # remove comments
+  end.reject do |s|
+    s.index('=').nil? # remove lines without =
+  end.inject({}) do |memento, line| # split on = and convert to hash
+    key, value = line.split '='
+    memento[key] = value
+    memento
   end
-
-  server_hash = Digest::MD5.hexdigest(server_url)
-  install_file = "#{Chef::Config[:file_cache_path]}/teamcity-agent-#{server_hash}.zip"
-  installed_check = Proc.new { ::File.exists? "#{system_dir}/bin" }
-
-  directory system_dir do
-    recursive true
-    action :create
+  if agent_name.nil? && !settings['name'].nil?
+    Chef::Log.info "Setting agent (#{agent_name})'s name to #{settings['name']}"
+    node.override['teamcity']['agents']['name'] = settings['name']
   end
-
-  remote_file install_file do
-    source server_url + '/update/buildAgent.zip'
-    action :create_if_missing
-    not_if &installed_check
+  if agent_auth_token.nil? && !settings['authorizationToken'].nil?
+    Chef::Log.info "Setting agent (#{agent_name})'s authorization_token"
+    node.override['teamcity']['agents']['authorization_token'] = settings['authorizationToken']
   end
+end
 
-  windows_zipfile system_dir do
-    source install_file
-    action :unzip
-    not_if &installed_check
-  end
+# buildAgent.properties (TeamCity will restart if this file is changed)
+template agent_config do
+  source 'buildAgent.properties.erb'
+  variables(
+    server_url: server_url,
+    name: node['teamcity']['agents']['name'],
+    work_dir: work_dir,
+    temp_dir: temp_dir,
+    system_dir: system_dir,
+    own_address: node['teamcity']['agents']['own_address'],
+    own_port: node['teamcity']['agents']['own_port'],
+    authorization_token: node['teamcity']['agents']['authorization_token'],
+    system_properties: node['teamcity']['agents']['system_properties'],
+    env_properties: node['teamcity']['agents']['env_properties']
+  )
+end
 
-  # try to extract agent name + authenticationCode from file
-  agent_config = ::File.join system_dir, 'conf', 'buildAgent.properties'
-  if (agent_name.nil? || agent_auth_token.nil?) && ::File.readable?(agent_config)
-    settings = File.new(agent_config).readlines.map do |s|
-      s.index('#') ? s.slice(0, s.index('#')).strip : s.strip  # remove comments
-    end.reject do |s|
-      s.index('=').nil? # remove lines without =
-    end.inject({}) do |memento, line| # split on = and convert to hash
-      key, value = line.split '='
-      memento[key] = value
-      memento
-    end
-    if agent_name.nil? && !settings['name'].nil?
-      Chef::Log.info "Setting agent (#{agent_name})'s name to #{settings['name']}"
-      node.override['teamcity']['agents']['name'] = settings['name']
-    end
-    if agent_auth_token.nil? && !settings['authorizationToken'].nil?
-      Chef::Log.info "Setting agent (#{agent_name})'s authorization_token"
-      node.override['teamcity']['agents']['authorization_token'] = settings['authorizationToken']
-    end
-  end
+# Service configuration file
+template "#{system_dir}/launcher/conf/wrapper.conf" do
+  source 'wrapper.conf.erb'
+  variables({ :name => agent_name,
+    :java_exe => java_exe })
+end
 
-  # buildAgent.properties (TeamCity will restart if this file is changed)
-  template agent_config do
-    source 'buildAgent.properties.erb'
-    variables(
-      server_url: server_url,
-      name: node['teamcity']['agents']['name'],
-      work_dir: work_dir,
-      temp_dir: temp_dir,
-      system_dir: system_dir,
-      own_address: node['teamcity']['agents']['own_address'],
-      own_port: node['teamcity']['agents']['own_port'],
-      authorization_token: node['teamcity']['agents']['authorization_token'],
-      system_properties: node['teamcity']['agents']['system_properties'],
-      env_properties: node['teamcity']['agents']['env_properties']
-    )
-  end
+# Install as Windows service
+execute "#{system_dir}/bin/service.install.bat" do
+  cwd "#{system_dir}/bin"
+  not_if { ::Win32::Service.exists?("TCBuildAgent_#{agent_name}") }
+end
 
-  # Service configuration file
-  template "#{system_dir}/launcher/conf/wrapper.conf" do
-    source 'wrapper.conf.erb'
-    variables({ :name => agent_name,
-      :java_exe => java_exe })
-  end
+# SC commands windows_service is not yet available for this
+ntservice_user = node['teamcity']['agent_windows']['ntservice_user']
+ntservice_password = node['teamcity']['agent_windows']['ntservice_password']
 
-  # Install as Windows service
-  execute "#{system_dir}/bin/service.install.bat" do
-    cwd "#{system_dir}/bin"
-    not_if { ::Win32::Service.exists?("TCBuildAgent_#{agent_name}") }
-  end
+# Stop the service
+execute "#{system_dir}/bin/service.stop.bat" do
+  cwd "#{system_dir}/bin"
+  only_if { ::Win32::Service.status("TCBuildAgent_#{agent_name}").current_state == 'running' &&
+    !ntservice_user.nil? }
+end
 
-  # SC commands windows_service is not yet available for this
-
-  ntservice_user = node['teamcity']['agent_windows']['ntservice_user']
-  ntservice_password = node['teamcity']['agent_windows']['ntservice_password']
-
-  # Stop the service
-
-  execute "#{system_dir}/bin/service.stop.bat" do
-    cwd "#{system_dir}/bin"
-    only_if { ::Win32::Service.status("TCBuildAgent_#{agent_name}").current_state == 'running' &&
-      !ntservice_user.nil? }
-  end
-
-  # Configure ntservice creds for service
-
-  execute 'configure-service' do
-    command "sc.exe config \"TCBuildAgent_#{agent_name}\" obj= \"#{ntservice_user}\" " \
-      "password= \"#{ntservice_password}\" type= own"
-    not_if { ntservice_user.nil? }
-  end
+# Configure ntservice creds for service
+execute 'configure-service' do
+  command "sc.exe config \"TCBuildAgent_#{agent_name}\" obj= \"#{ntservice_user}\" " \
+    "password= \"#{ntservice_password}\" type= own"
+  not_if { ntservice_user.nil? }
+end
 
 
-  # Start the service
-  execute "#{system_dir}/bin/service.start.bat" do
-    cwd "#{system_dir}/bin"
-    only_if { ::Win32::Service.status("TCBuildAgent_#{agent_name}").current_state != 'running' }
-  end
-
-# end
+# Start the service
+execute "#{system_dir}/bin/service.start.bat" do
+  cwd "#{system_dir}/bin"
+  only_if { ::Win32::Service.status("TCBuildAgent_#{agent_name}").current_state != 'running' }
+end
