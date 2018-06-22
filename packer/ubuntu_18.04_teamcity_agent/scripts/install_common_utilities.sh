6e00393de3897d58c08ceafbd92474fb94b4e756
Linus Ruth <linus.ruth@changepoint.com>
2018-06-22 15:56:36 -0700

    chore: add script to install utilities in ami

---
+++ packer/ubuntu_18.04_teamcity_agent/scripts/install_common_utilities.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Determining Linux distribution..."
+if [ -x "$(command -v apt-get)" ]; then
+  echo "Found apt-get, assuming Debian family..."
+  export DISTRO="debian"
+elif [ -x "$(command -v yum)" ]; then
+  echo "Found yum, assuming Red Hat family..."
+  export DISTRO="redhat"
+else
+  echo "Unable to determine Linux distribution."
+  exit 1
+fi
+
+echo "Installing curl..."
+case $DISTRO in
+  "debian")
+    sudo apt-get install -y curl
+    ;;
+  "redhat")
+    sudo yum install -y curl
+    ;;
+esac
+
+echo "Installing jq..."
+case $DISTRO in
+  "debian")
+    sudo apt-get install -y jq
+    ;;
+  "redhat")
+    sudo yum install -y jq
+    ;;
+esac
+
+echo "Installing python-pip..."
+case $DISTRO in
+  "debian")
+    sudo apt-get install -y python-pip
+    ;;
+  "redhat")
+    sudo yum install -y python-pip
+    ;;
+esac
+
+echo "Installing aws-cli..."
+sudo pip install --upgrade awscli
