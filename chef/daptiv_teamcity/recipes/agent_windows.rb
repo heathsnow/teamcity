31f537e28d387ec2e9fd9a40db6b41243c16ab2c
Heath Snow <heathsnow@gmail.com>
2019-07-29 11:46:50 -0700

    stop using ntservice for teamcity

--- chef/daptiv_teamcity/recipes/agent_windows.rb
+++ chef/daptiv_teamcity/recipes/agent_windows.rb
@@ -18,13 +18,14 @@ node.override['teamcity']['agents']['server_url'] =
 node.override['teamcity']['agents']['system_dir'] =
   node['daptiv_teamcity']['windows']['system_dir']
 
+# TeamCity Cloud agent should just use system account
 # Set the service user using data bag tc_userdata
-unless node.chef_environment == 'cookbook_ci'
-  node.override['teamcity']['agent_windows']['ntservice_user'] =
-    tc_userdata['domain_username']
-  node.override['teamcity']['agent_windows']['ntservice_password'] =
-    tc_userdata['domain_password']
-end
+# unless node.chef_environment == 'cookbook_ci'
+  # node.override['teamcity']['agent_windows']['ntservice_user'] =
+    # tc_userdata['domain_username']
+  # node.override['teamcity']['agent_windows']['ntservice_password'] =
+    # tc_userdata['domain_password']
+# end
 
 # SSO cookbook for testagents use 9090 so need to update TC agent port to
 # run on 9000 intead of the 9090 default
