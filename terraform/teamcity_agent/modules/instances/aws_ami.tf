7957f9974b94dee53f02bd37eab4304914e0a258
Heath Snow <heathsnow@gmail.com>
2019-08-26 16:25:11 -0700

    remove terraform agent deploy....no longer needed

--- terraform/teamcity_agent/modules/instances/aws_ami.tf
+++
@@ -1,8 +0,0 @@
-data "aws_ami" "my_ami" {
-  most_recent = true
-  owners = ["${split(",", var.ami_owners)}"]
-  filter {
-    name = "name"
-    values = ["${var.ami_name}"]
-  }
-}
--- terraform/teamcity_agent/modules/instances/files/knife.tpl
+++
@@ -1,4 +0,0 @@
-log_location STDOUT
-node_name "${chef_user_name}"
-chef_server_url "${chef_server_url}"
-client_key "#{File.dirname(__FILE__)}/${chef_user_name}.pem"
